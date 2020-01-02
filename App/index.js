58565e72f776fbbf74040c473310160f229db375
SawadaSaiko <sawarafael@gmail.com>
2020-01-02 20:05:45 -0300

    organização repo app

---
+++ App/index.js
@@ -0,0 +1,46 @@
+const {app, BrowserWindow, ipcMain, Menu} = require('electron');
+const path = require('path');
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    resizable: true,
+    fullscreen: false,
+    autoHideMenuBar: true,
+    frame: true,
+    backgroundColor: '#FFF',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('login.html')
+
+  mainWindow.on('closed', function () {
+  
+    mainWindow = null
+  })
+}
+app.on('ready', createWindow)
+
+
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+
+app.on('activate', function () {
+ 
+  if (mainWindow === null) createWindow()
+})
+
+
+
+
+
