71d67781e114bcd6998872497fed4e5129e92539
Isabela Lima <isabelamslimas@gmail.com>
2020-03-21 19:43:59 -0300

    Correções

--- App/main.js
+++ App/main.js
@@ -20,7 +20,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("./public/views/login.html");
+  mainWindow.loadFile("./public/views/perfil.html");
 
   mainWindow.on("closed", function() {
     mainWindow = null;
