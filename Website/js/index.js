a1717beca469c346b010d691878ad525630a457b
SawadaSaiko <sawarafael@gmail.com>
2020-05-29 20:12:07 -0300

    Correção de repositório

--- Website/js/index.js
+++
@@ -1,4 +0,0 @@
-$( '#topheader .navbar-nav a' ).on( 'click', function () {
-	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-});
