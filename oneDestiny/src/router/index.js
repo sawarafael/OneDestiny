b385762850908297fd551165fab2fc418c43740b
SawadaSaiko <sawarafael@gmail.com>
2020-08-23 08:52:34 -0300

    Protótipo da Tela de Notícias criada; Possuindo e testando conexões e consumos de uma API nos valores atribuidos finalizado.

--- oneDestiny/src/router/index.js
+++ oneDestiny/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import News from '@/components/News'
+import Details from '@/components/Details'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/noticias',
       name: 'News',
       component: News
+    },
+     {
+      path: '/artigo',
+      name: 'Details',
+      component: Details
     }
+     
+
   ]
 })
