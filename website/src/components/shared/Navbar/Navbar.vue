d93cf0d269414f8ccb5578c98c753bc9b77a9b80
Isabela Lima <isabelamslimas@gmail.com>
2020-08-26 21:09:00 -0300

    mudanças na navbar

--- website/src/components/shared/Navbar/Navbar.vue
+++ website/src/components/shared/Navbar/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
   <nav>
     <v-app-bar flat app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-app-title class="text-uppercase">
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        v-click-outside="onClickOutside"
+      ></v-app-bar-nav-icon>
+      <v-app-title class="text-uppercase text-h6">
         <span class="font-weight-light">One</span>
         <span>Destiny</span>
       </v-app-title>
@@ -13,7 +16,49 @@
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app class="primary"></v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-list dark>
+        <v-subheader class="font-weight-light text-uppercase"
+          >Navegação</v-subheader
+        >
+        <v-list-item-group>
+          <v-list-item to="Home">
+            <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium text-uppercase"
+                >Home</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="Blog">
+            <v-list-item-icon><v-icon>mdi-blogger</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium text-uppercase"
+                >Blog</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="News">
+            <v-list-item-icon><v-icon>mdi-newspaper</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium text-uppercase"
+                >News</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="AboutUs">
+            <v-list-item-icon
+              ><v-icon>mdi-account-group</v-icon></v-list-item-icon
+            >
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-medium text-uppercase"
+                >About Us</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -21,9 +66,14 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
     };
-  }
+  },
+  methods: {
+    onClickOutside() {
+      this.drawer = false;
+    },
+  },
 };
 </script>
 
--- website/src/views/AboutUs.vue
+++ website/src/views/AboutUs.vue
@@ -1,5 +1,32 @@
 <template>
-  <div></div>
+  <v-app>
+    <v-content>
+      <v-row class="corpo">
+        <v-col
+          ><p class="text-center text-h2 font-weight-thin">Quem somos nós?</p>
+          <p class="text-h3 font-weight-thin text-light">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum
+            maiores modi quidem veniam, expedita quis laboriosam, ullam facere
+            adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo
+            numquam fuga ab at.
+          </p>
+          <p class="text- font-weight-medium text-uppercase">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At porro
+            iste officia nihil possimus nobis neque fugit odit hic. Voluptatibus
+            reprehenderit aut laborum amet ducimus facere ex tempore eligendi
+            veritatis!
+          </p></v-col
+        >
+      </v-row>
+      <v-row class="footer">
+        <v-col>
+          <p class="text-h5 font-weight-thin text-left">
+            O que fazemos?
+          </p>
+        </v-col>
+      </v-row>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -9,4 +36,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.corpo {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.footer {
+  margin: 0 auto;
+}
+</style>
